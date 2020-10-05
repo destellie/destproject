ad7cb2badc0629afd07ccda4956cc24556759ca1
destellie <justin.destellie@gmail.com>
2020-10-05 12:43:36 +0000

    new update repository1

--- database/migrations/2020_09_25_233424_create_foreign_key_for_role_user_table.php
+++ database/migrations/2020_09_25_233424_create_foreign_key_for_role_user_table.php
@@ -16,7 +16,7 @@ class CreateForeignKeyForRoleUserTable extends Migration
         Schema::table('role_user', function (Blueprint $table) {
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->timestamps();
+        
         });
     }
 
